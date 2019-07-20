ce1930918cb36a11edba95d0947e308333344451
Xuanyu Li <li.xua@husky.neu.edu>
2019-07-20 00:29:33 -0400

    add hello first http

--- product/models.py
+++ product/models.py
@@ -1,46 +1,51 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+
 class User(models.Model):
-    firstName = models.CharField(max_length = 30)
-    lastName = models.CharField(max_length = 30)
-    userName = models.CharField(max_length = 30)
-    password = models.CharField(max_length = 30)
-    email = models.EmailField(unique = True)
-    phone = PhoneNumberField(unique = True)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    userName = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone = PhoneNumberField(unique=True)
     createDate = models.DateTimeField(auto_now_add=True)
     updateDate = models.DateTimeField(auto_now=True)
 
+
 class Transaction(models.Model):
-    fromUser = models.ForeignKey(User, on_delete= models.DO_NOTHING, related_name = 'transactionFromUser')
-    toUser = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name = 'transactionToUser')
+    fromUser = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='transactionFromUser')
+    toUser = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='transactionToUser')
     value = models.FloatField()
-    title = models.CharField(max_length = 30)
-    comment = models.CharField(max_length = 200)
-    TRANSCATION_STATUS = (
+    title = models.CharField(max_length=30)
+    comment = models.CharField(max_length=200)
+    TRANSACTION_STATUS = (
         (0, 'CREATE'),
         (1, 'ACCEPT'),
         (2, 'DENY'),
         (3, 'FINISH'),
     )
-    status = models.IntegerField(choices=TRANSCATION_STATUS, default = 0)
+    status = models.IntegerField(choices=TRANSACTION_STATUS, default=0)
     createDate = models.DateTimeField(auto_now_add=True)
     updateDate = models.DateTimeField(auto_now=True)
 
+
 class Connection(models.Model):
-    fromUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'connectionFromUser')
-    toUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'connectionToUser')
+    fromUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='connectionFromUser')
+    toUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='connectionToUser')
     CONNECTION_STATUS = {
         (0, 'REQUEST'),
         (1, 'ACCEPT'),
         (2, 'BLOCK'),
     }
-    status = models.IntegerField(choices=CONNECTION_STATUS, default = 0)
+    status = models.IntegerField(choices=CONNECTION_STATUS, default=0)
     createDate = models.DateTimeField(auto_now_add=True)
     updateDate = models.DateTimeField(auto_now=True)
 
+
 class Chat(models.Model):
-    title = models.CharField(max_length = 30)
+    title = models.CharField(max_length=30)
     listUser = models.ManyToManyField(
         User,
     )
--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+from product.models import User
+from product.models import Transaction
+from product.models import Connection
+from product.models import Chat
+
+
+def hello(request):
+    return HttpResponse()
