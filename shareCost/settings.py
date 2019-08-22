03b7f99fb176bfe3dfdf0e14d6271ce6903877c3
Xuanyu Li <li.xua@husky.neu.edu>
2019-08-21 21:49:54 -0400

    test

--- shareCost/settings.py
+++ shareCost/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
